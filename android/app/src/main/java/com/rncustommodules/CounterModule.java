996e4d9d443b7ebe0331fd4069610f70bef93726
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-03-31 13:50:07 +0300

    Implement Android Counter

---
+++ android/app/src/main/java/com/rncustommodules/CounterModule.java
@@ -0,0 +1,50 @@
+package com.rncustommodules;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import javax.annotation.Nonnull;
+
+public class CounterModule extends ReactContextBaseJavaModule {
+    private int count = 0;
+
+    public CounterModule(@Nonnull ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "Counter";
+    }
+
+    @ReactMethod
+    public void increment() {
+        count += 1;
+        sendOnChangeEvent();
+    }
+
+    @ReactMethod
+    public void getCount(Promise promise) {
+        promise.resolve(count);
+    }
+
+    @ReactMethod
+    public void setCount(int value) {
+        count = value;
+        sendOnChangeEvent();
+    }
+
+    private void sendOnChangeEvent() {
+        this.getReactApplicationContext()
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit("onChange", count);
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  NativeModules,
+  DeviceEventEmitter
+} from "react-native";
 
 import Instructions from "./Instructions";
 import Button from "./Button";
 
+const { Counter } = NativeModules;
+
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [nativeCount, setNativeCount] = useState(0);
+
+  const updateJavaScriptCounter = async () => {
+    setCount(await Counter.getCount());
+  };
+
+  const updateNativeCounter = () => {
+    Counter.setCount(count);
+  };
+
+  useEffect(() => {
+    const setNativeCounter = value => {
+      setNativeCount(value);
+    };
+
+    DeviceEventEmitter.addListener("onChange", setNativeCounter);
+
+    return function cleanup() {
+      DeviceEventEmitter.removeListener("onChange", setNativeCounter);
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Button onPress={() => {}} value={0} color="dodgerblue">
+      <Button
+        onPress={() => setCount(count + 1)}
+        onLongPress={updateNativeCounter}
+        value={count}
+        color="dodgerblue"
+      >
         JavaScript counter
       </Button>
 
       <Instructions />
 
-      <Button onPress={() => {}} value={0} color="orange">
+      <Button
+        onPress={() => Counter.increment()}
+        onLongPress={updateJavaScriptCounter}
+        value={nativeCount}
+        color="orange"
+      >
         Native counter
       </Button>
     </View>
